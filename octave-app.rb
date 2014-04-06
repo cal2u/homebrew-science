1629915b7c6f553c8d148563b060a51110664bb4
Sebastian Schöps <sebastian@schoeps.org>
2014-04-06 19:13:08 +0200

    now using applescript as launcher

--- octave-app.rb
+++ octave-app.rb
@@ -11,39 +11,42 @@ class OctaveApp < Formula
   keg_only "This is only an empty app bundle calling Octave."
 
   def install
-    system('mkdir',"-p","#{buildpath}/Octave.app/Contents/MacOS/")
-    exe = File.open('Octave.app/Contents/MacOS/Octave', 'w')
-    exe.write("#!/bin/bash\n")
-    exe.write("export PATH=#{HOMEBREW_PREFIX}/bin:#{HOMEBREW_PREFIX}/sbin:${PATH}\n")
-    exe.write("exec #{HOMEBREW_PREFIX}/bin/octave -i --force-gui | logger 2>&1\n")
+    #create applescript to start octave
+    exe = File.open("Octave.applescript", 'w')
+    exe.write('--
+    on open argv
+      set filename to "\'" & POSIX path of item 1 of argv & "\'"
+      do shell script "PATH=/usr/local/bin/:$PATH;cd ~;/usr/local/bin/octave --force-gui --persist --eval \"edit " & filename & "\" | logger 2>&1"
+    end open
+    on run
+    do shell script "PATH=/usr/local/bin/:$PATH;cd ~;/usr/local/bin/octave --force-gui  | logger 2>&1"
+    end run')
     exe.close()
-    system('chmod','a=rx','Octave.app/Contents/MacOS/Octave')
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleDevelopmentRegion","English")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleDisplayName","Octave")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleExecutable","Octave")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleIconFile","appIcon.icns")
+    #create appbundle
+    system('osacompile',"-oOctave.app","Octave.applescript")
+    #modify some entries in the plist 
+    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","NSUIElement","1")
     system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleIdentifier","org.octave.Octave")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleInfoDictionaryVersion","6.0")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleName","Octave")
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundlePackageType","APPL")
     system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleShortVersionString", Formula['Octave'].version)
     system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","CFBundleVersion", Formula['Octave'].version)
-    system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","LSMinimumSystemVersion","10.9.1")
     system('defaults','write',"#{buildpath}/Octave.app/Contents/Info","NSHumanReadableCopyright","© 2014 JWE and others")
+    system("defaults write #{buildpath}/Octave.app/Contents/Info CFBundleDocumentTypes -array \'{ \"CFBundleTypeExtensions\" = (\"m\"); \"CFBundleTypeOSTypes\" = (\"Mfile\"); \"CFBundleTypeRole\" = \"Editor\";}\'")    
     system('plutil','-convert','xml1',"#{buildpath}/Octave.app/Contents/Info.plist")
-    system('mkdir',"-p","#{buildpath}/appIcon.iconset")
-    system('mkdir',"-p","#{buildpath}/Octave.app/Contents/Resources/")
-    system("sips -z 16 16 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_16x16.png")
-    system("sips -z 32 32 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_16x16@2x.png")
-    system("sips -z 32 32 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_32x32.png")
-    system("sips -z 64 64 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_32x32@2x.png")
-    system("sips -z 128 128 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_128x128.png")
-    system("sips -z 256 256 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_128x128@2x.png")
-    system("sips -z 256 256 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_256x256.png")
-    system("sips -z 1024 1024 #{buildpath}/Icon.png --out #{buildpath}/appIcon.iconset/icon_512x512@2x.png")
-    system('cp','Icon.png',"#{buildpath}/appIcon.iconset/icon_256x256@2x.png")
-    system('mv','Icon.png',"#{buildpath}/appIcon.iconset/icon_512x512.png")
-    system("iconutil -c icns -o #{buildpath}/Octave.app/Contents/Resources/appIcon.icns #{buildpath}/appIcon.iconset")
+    system('chmod','a=r',"#{buildpath}/Octave.app/Contents/Info.plist")
+    #create a nice icon
+    system('mkdir',"-p","droplet.iconset")
+    system("sips -z 16 16 Icon.png --out droplet.iconset/icon_16x16.png")
+    system("sips -z 32 32 Icon.png --out droplet.iconset/icon_16x16@2x.png")
+    system("sips -z 32 32 Icon.png --out droplet.iconset/icon_32x32.png")
+    system("sips -z 64 64 Icon.png --out droplet.iconset/icon_32x32@2x.png")
+    system("sips -z 128 128 Icon.png --out droplet.iconset/icon_128x128.png")
+    system("sips -z 256 256 Icon.png --out droplet.iconset/icon_128x128@2x.png")
+    system("sips -z 256 256 Icon.png --out droplet.iconset/icon_256x256.png")
+    system("sips -z 1024 1024 Icon.png --out droplet.iconset/icon_512x512@2x.png")
+    system('cp','Icon.png',"droplet.iconset/icon_256x256@2x.png")
+    system('mv','Icon.png',"droplet.iconset/icon_512x512.png")
+    system("iconutil -c icns -o #{buildpath}/Octave.app/Contents/Resources/droplet.icns #{buildpath}/droplet.iconset")
+    #install appbundle
     prefix.install "Octave.app"
   end
 
