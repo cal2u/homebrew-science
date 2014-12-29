13f11e3caeb1bbd878d670c2f32c8e4e8aaa5c88
Sebastian Schöps <sebastian@schoeps.org>
2014-12-29 20:47:50 +0100

    New version, using internal ruby commands and fixed location of onelab.py

--- gmsh-app.rb
+++ gmsh-app.rb
@@ -2,23 +2,23 @@ require 'formula'
 
 class GmshApp < Formula
   homepage 'http://geuz.org/gmsh'
-  url 'http://geuz.org/gmsh/src/gmsh-2.8.4-source.tgz'
-  sha1 'e96209c46874cb278e2028933871c7e7d60e662d'
+  url 'http://geuz.org/gmsh/src/gmsh-2.8.5-source.tgz'
+  sha1 '352671f95816440ddb2099478f3e9f189e40e27a'
 
   depends_on "gmsh" => "with-fltk"
-  
+
   def install
-        system "mkdir", "-p", "GMSH.app/Contents/MacOS" 
-        system "mkdir", "-p", "GMSH.app/Contents/Resources" 
-        system "cp", "Fltk/MacIcons.icns", "GMSH.app/Contents/Resources/Gmsh.icns" 
-        system "cp", "Fltk/MacIconsGeo.icns", "GMSH.app/Contents/Resources/GmshGeo.icns" 
-        system "cp", "Fltk/MacIconsMsh.icns", "GMSH.app/Contents/Resources/GmshMsh.icns" 
-        system "cp", "Fltk/MacIconsSol.icns", "GMSH.app/Contents/Resources/GmshSol.icns" 
-        system "cp", "Fltk/MacIconsPos.icns", "GMSH.app/Contents/Resources/GmshPos.icns" 
-        system "cp", "utils/misc/gmsh_app.plist",  "GMSH.app/Contents/Info.plist"
-        inreplace "GMSH.app/Contents/Info.plist", "GMSH_VERSION", Formula['gmsh'].version        
-        system "ln", "-s", "#{HOMEBREW_PREFIX}/bin/gmsh", "GMSH.app/Contents/MacOS/gmsh"
-        system "ln", "-s", "#{HOMEBREW_PREFIX}/bin/onelab.py", "GMSH.app/Contents/MacOS/onelab.py"
+        mkdir_p "GMSH.app/Contents/MacOS"
+        mkdir_p "GMSH.app/Contents/Resources"
+        cp "Fltk/MacIcons.icns", "GMSH.app/Contents/Resources/Gmsh.icns"
+        cp "Fltk/MacIconsGeo.icns", "GMSH.app/Contents/Resources/GmshGeo.icns"
+        cp "Fltk/MacIconsMsh.icns", "GMSH.app/Contents/Resources/GmshMsh.icns"
+        cp "Fltk/MacIconsSol.icns", "GMSH.app/Contents/Resources/GmshSol.icns"
+        cp "Fltk/MacIconsPos.icns", "GMSH.app/Contents/Resources/GmshPos.icns"
+        cp "utils/misc/gmsh_app.plist",  "GMSH.app/Contents/Info.plist"
+        inreplace "GMSH.app/Contents/Info.plist", "GMSH_VERSION", Formula['gmsh'].version
+        ln_s "#{HOMEBREW_PREFIX}/bin/gmsh", "GMSH.app/Contents/MacOS/gmsh"
+        ln_s "#{Formula["gmsh"].opt_libexec}/onelab.py", "GMSH.app/Contents/MacOS/onelab.py"
         #install appbundle
         prefix.install "GMSH.app"
     end
