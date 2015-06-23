9a9c0cdd17359002255fa0dd84f0fbdc3e52ce82
Sebastian Schöps <sebastian@schoeps.org>
2015-06-23 20:08:11 +0200

    version bumb to 1.66

--- R-app.rb
+++ R-app.rb
@@ -3,10 +3,8 @@ require 'formula'
 class RApp < Formula
     
     homepage 'http://cran.r-project.org/bin/macosx/'
-    url 'http://cran.r-project.org/bin/macosx/Mac-GUI-1.65.tar.gz'
-    sha256 '1d8643c91e588da9df6e5b9b6471dc42ba8b080f3aab258e5443af5157bde79b'
-    
-    head 'https://svn.r-project.org/R-packages/trunk/Mac-GUI'
+    url 'http://cran.r-project.org/bin/macosx/Mac-GUI-1.66.tar.gz'
+    sha256 'dc90e0ebb44d7ffa04fb804a145a477518e38b2e45ddbd64a2bcbd8a5067cc4a'
     
     depends_on :xcode => :build
     depends_on 'r'
@@ -18,13 +16,6 @@ end
 def install
     if !File.exist?("/Library/Frameworks/R.framework")
         onoe "Framework \"/Library/Frameworks/R.framework\" not found. Create a link:\n\nsudo ln -s \"#{HOMEBREW_PREFIX}/opt/r/R.framework\" \"/Library/Frameworks/R.framework\"\n \n"
-        else
-        ohai "Linking to Framework \"/Library/Frameworks/R.framework\"."
-    end
-    
-    # ugly hack to get updateSVN script in build to not fail
-    if build.head?
-        cp_r cached_download/".svn", buildpath
     end
     
     xcodebuild "-target", "R", "SYMROOT=build"
