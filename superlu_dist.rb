10a49caf0d03a028da9804da7a0d06bf37f1826e
Sebastian Schöps <schoeps@temf.tu-darmstadt.de>
2017-11-02 17:00:22 +0100

    petsc dependencies

---
+++ superlu_dist.rb
@@ -0,0 +1,64 @@
+class SuperluDist < Formula
+  desc "Distributed LU factorization for large linear systems"
+  homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/"
+  url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_dist_5.1.0.tar.gz"
+  sha256 "30ac554a992441e6041c6fb07772da4fa2fa6b30714279de03573c2cad6e4b60"
+  revision 2
+
+  bottle do
+    sha256 "cd577c006da8f90d6835fbe6351fcd8027dcbd7270bb870434a2e2b1350232e4" => :sierra
+    sha256 "d96ec784ec58b0b40ba07ed784301d2d5bed4b9323eecc50aae06fc8ed6647be" => :el_capitan
+    sha256 "93998ea9efe9b1bbc48adcd839936790f2970788848ed4e94214cfdd66d1ac9a" => :yosemite
+    sha256 "d188e92bd33981ecbbea58b75c1b044dbcd574e195507c8409a2ebd075f297be" => :x86_64_linux
+  end
+
+  option "without-test", "skip build-time tests (not recommended)"
+  option "with-openmp", "Enable OpenMP multithreading"
+  needs :openmp if build.with? "openmp"
+
+  depends_on "cmake" => :build
+  depends_on :fortran
+  depends_on :mpi => [:cc, :f77, :f90]
+
+  depends_on "parmetis"
+  depends_on "openblas" => OS.mac? ? :optional : :recommended
+  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
+
+  def install
+    # prevent linking errors on linuxbrew:
+    ENV.deparallelize
+
+    dylib_ext = OS.mac? ? "dylib" : "so"
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DTPL_PARMETIS_LIBRARIES=#{Formula["parmetis"].opt_lib}/libparmetis.#{dylib_ext};#{Formula["metis"].opt_lib}/libmetis.#{dylib_ext}"
+    cmake_args << "-DTPL_PARMETIS_INCLUDE_DIRS=#{Formula["parmetis"].opt_include};#{Formula["metis"].opt_include}"
+    cmake_args << "-DCMAKE_C_FLAGS=-fPIC -O2"
+    cmake_args << "-DBUILD_SHARED_LIBS=ON"
+    cmake_args << "-DCMAKE_C_COMPILER=#{ENV["MPICC"]}"
+    cmake_args << "-DCMAKE_Fortran_COMPILER=#{ENV["MPIF90"]}"
+    cmake_args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+
+    blaslib = ((build.with? "openblas") ? "-L#{Formula["openblas"].opt_lib} -lopenblas" : "-L#{Formula["veclibfort"].opt_lib} -lvecLibFort")
+    cmake_args << "-DTPL_BLAS_LIBRARIES=#{blaslib}"
+
+    mkdir "build" do
+      system "cmake", "..", *cmake_args
+      system "make"
+      system "make", "install"
+      system "make", "test" if build.with? "test"
+    end
+
+    doc.install "DOC/ug.pdf"
+    pkgshare.install "EXAMPLE"
+  end
+
+  test do
+    ENV.fortran
+    cp pkgshare/"EXAMPLE/dcreate_matrix.c", testpath
+    cp pkgshare/"EXAMPLE/pddrive.c", testpath
+    cp pkgshare/"EXAMPLE/g20.rua", testpath
+    system "mpicc", "-o", "pddrive", "pddrive.c", "dcreate_matrix.c", "-L#{Formula["superlu_dist"].opt_lib}", "-lsuperlu_dist"
+    system "mpirun", "-np", "4", "./pddrive", "-r", "2", "-c", "2", "g20.rua"
+  end
+end
